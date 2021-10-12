5f13ccc112bbefb1195947fbea333d6de4602465
Ariel-ttw <61923883+Ariel-ttw@users.noreply.github.com>
2021-10-12 14:45:51 +1000

    Add files via upload

---
+++ src/index.php
@@ -0,0 +1,8 @@
+<?php 
+include_once 'header.php';
+echo "I am a PHP page running on four containers: NginX + PHP-PFM + MariaDB + Redis!";
+?>
+
+    <section class='container'>
+        <h1> Some random things </h1>
+    </section>
